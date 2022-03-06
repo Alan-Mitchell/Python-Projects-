5e55446048c7b2637814caa7098e3ab39767ced6
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-03-06 08:00:23 -0600

    Submission database

--- DATABASE_SUBMISSION_ASSIGNMENT.py
+++ DATABASE_SUBMISSION_ASSIGNMENT.py
@@ -5,7 +5,7 @@ conn = sqlite3.connect('submission.db')
 #conditional statment to access database and modify
 with conn:
     cur = conn.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS tbl_exapmle1(\
+    cur.execute('CREATE TABLE IF NOT EXISTS tbl_example1(\
         ID INTEGER PRIMARY KEY AUTOINCREMENT,\
         col_fname TEXT,\
         col_lname TEXT,\
@@ -21,8 +21,23 @@ with con:
     cur.execute('INSERT INTO tbl_example1(col_fname,col_lname,col_email) VALUES(?,?,?)',\
                 ('Alan','Mitchell','am@aol.com'))
     cur.execute('INSERT INTO tbl_example1(col_fname,col_lname,col_email) VALUES(?,?,?)',\
-                ('Jane','Doe','jd@gmail.com'))
+                ('John','Smith','js@aol.com'))
     cur.execute('INSERT INTO tbl_example1(col_fname,col_lname,col_email) VALUES(?,?,?)',\
-                ('John','Smith','js@hotmail.com'))
+                ('Jane','Doe','jd@aol.com')) 
+    
+   
     conn.commit()
-conn.close()    
+conn.close()
+
+import os
+
+fileList =('information.docx','Hello.txt','myImage.png',\
+           'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+ext='.txt'
+
+all_txt=[ i for i in fileList if os.path.splitext(i)[1] == ext]
+
+print(all_txt)
+
+
+
