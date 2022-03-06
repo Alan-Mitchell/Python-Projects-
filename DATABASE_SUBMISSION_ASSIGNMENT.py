c7ce16ff172f24a4a6c7d5bb56cb2fed34a43c2d
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-03-06 08:50:20 -0600

    update

--- DATABASE_SUBMISSION_ASSIGNMENT.py
+++ DATABASE_SUBMISSION_ASSIGNMENT.py
@@ -41,3 +41,5 @@ print(all_txt)
 
 
 
+
+
---
+++ class&object.py
@@ -0,0 +1,15 @@
+
+class Patient:
+    name = 'Provide Name'
+    Age= '32'
+    Insurance= 'United'
+    Payment_type='Credit'
+
+class New(Patient):
+    zip_code= 38301
+    email = ''
+
+class Existing(Patient):
+    Remaining_balance = 2000
+    emergencyContact = ''
+    
