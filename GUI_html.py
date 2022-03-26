d481fc684ee90d604be810a0d11c20c29fc0fea7
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-03-25 20:56:38 -0500

    update

--- GUI_html.py
+++ GUI_html.py
@@ -1,4 +1,19 @@
+#import everything from tkinter
 from tkinter import *
-tkinter=''
-tkinter.append('')
-tkinter.simpledialog.askstring(title, prompt, **kw)
+
+#Create window object
+window=Tk()
+
+#define text box
+la=Label(window, text = 'Body')
+la.grid(row=1,column=1)
+
+#define entries
+body_input=StringVar()
+e=Entry(window,textvariable=body_input)
+e.grid(row=0, column=1)
+
+
+window.mainloop()
+
+
--- web_page_generator.py
+++ web_page_generator.py
@@ -2,21 +2,43 @@
 import os
 import webbrowser
 from tkinter import *
-#variable to create html file
-htmlPage= open('Summer.html','w')
-# variable for HTML contents
-message='''
 
-<html>
-    <head></head>
-        <body>
-        <h1>Stay tuned for our amazing summer sale!</h1>
-        </body>
-</html>
-'''
-#methods used to control HTML file 
-htmlPage.write(message)
-htmlPage.close()
-webbrowser.open_new_tab('Summer.html')
+#create window object
+window=Tk()
 
-tkinter.simpledialog.askstring(title, prompt, **kw)
+#define text box
+la=Label(window, text = 'Body')
+la.grid(row=1, column=1)
+
+#define entries
+body_input=StringVar()
+e=Entry(window,textvariable=body_input)
+e.grid(row=0,column=1)
+
+def Submit():
+    text=e.get()
+    html_template = '''
+    <html>
+    <body>
+    <h1>
+    {}
+    </h1>
+    </body>
+    </html>
+    '''.format(text)
+    f= open('Summer.html','w')
+    print(html_template)
+    #writing code into file
+    f.write(html_template)
+    f.close()
+    webbrowser.open_new_tab('Summer.html')
+
+#define buttons
+b=Button(window,text = 'Submit',command = lambda:Submit())
+b.grid(row=2, column=3)
+
+
+
+window.mainloop()
+
+
