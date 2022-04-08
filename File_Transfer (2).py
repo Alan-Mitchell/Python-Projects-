5f7621a4203ec3394ee14e670529f2adbb34a505
Alan-Mitchell <94767396+Alan-Mitchell@users.noreply.github.com>
2022-04-08 16:53:42 -0500

    update

--- File_Transfer (2).py
+++ File_Transfer (2).py
@@ -1,31 +1,32 @@
+#importing all the needed modules 
 import tkinter
 from tkinter import *  
 from tkinter import filedialog as fd
-from os import * as os
+import os
 import datetime as dt
 import shutil
-
+#function to source
 def getSource():
     mydir = fd.askdirectory()
     srcdir.delete(0,END)
     srcdir.insert(0,mydir)
-
+#function to destination
 def getDestinatiom():
     newdir = fd.askdirectory()
     newloc.delete(0,END)
     newloc.insert(0,newdir)
-
+#functon for file transfer
 def file_transfer():
     now = dt.datetime.now()
     ago = now-dt.timedelta(hours=24)
     strftime = "%H:%M %m/%d/%Y"
-    created = \Users\Admin\Desktop\FolderA
-    dest = C:\Users\Admin\Desktop\FolderB
-    for m, in os.walk(created):  
+    created =srcdir.get()
+    dest = newloc.get()
+    files= os.listdir(created)
     for fname in files:
-        path = os.path.join(m, fname)
-        st = os.stat(path)    
-        mtime = dt.datetime.fromtimestamp(st.st_mtime)
+        path = os.path.join(created, fname)
+        st = os.path.getmtime(path)   
+        mtime = dt.datetime.fromtimestamp(st)
         if mtime > ago:
             print("True:  ", fname, " at ", mtime.strftime("%H:%M %m/%d/%Y"))
             shutil.move(path, dest)
